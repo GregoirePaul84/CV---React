aabfd8c3c1e6048ad66c0a4997eb5e6ece413710
Grégoire Paulet <gregoirepaulet84@gmail.com>
2022-09-08 10:07:16 +0200

    réalisation de la pluie sur la page skills

--- react-cv/src/pages/Skills.js
+++ react-cv/src/pages/Skills.js
@@ -4,43 +4,76 @@ import Navbar from '../components/Navbar';
 
 import vietnam from '../images/vietnam.jpg';
 import vietnamCloud from '../images/cloud_vietnam.png';
-// import rain from '../images/rain.png';
+
+let timeSkills = 0;
+let timeBlur = 0;
 
 const Skills = () => {
 
     useEffect(() => {
 
         const selectRainy = document.querySelector('.background-gray');
+        const selectRainFall = document.querySelector('.background-rain');
+        const selectBgImg = document.querySelector('.bg-img');
 
-        let timeSkills = 0;
+        function rainFall() {
+            const waterDrop = document.createElement('i');
 
-        function displayRainyDay() {
-            selectRainy.classList.add('active-background-gray');
+            waterDrop.classList.add('fas');
+	        waterDrop.classList.add('fa-tint');
+
+            waterDrop.style.left = Math.random() * window.innerWidth + 'px';
+
+            waterDrop.style.opacity = Math.random() + 0.4;
+	        waterDrop.style.fontSize = Math.random() * 7 +'px';
+
+            waterDrop.style.animationDuration = Math.random() * 1.5 + 's';
+
+            selectRainFall.appendChild(waterDrop);
+
+            setTimeout(() => {
+                waterDrop.remove();
+            }, 2000)
         }
 
         function increaseTime() {
             timeSkills ++;
 
             if (timeSkills === 15) {
-                displayRainyDay();
+                selectRainy.classList.remove('inactive-background-gray');
+                selectRainy.classList.add('active-background-gray');
+                
             }
 
-            else if (timeSkills === 50) {
+            else if (timeSkills === 20) {
+                console.log('pluie');
+                setInterval(rainFall, 10);
+
+                if (timeBlur >= 0 && timeBlur <= 3) {
+                    setInterval(timeBlur ++, 1000);
+                    setInterval(console.log(timeBlur), 1000);
+                }
+
+                if (timeBlur === 3) {
+                    selectBgImg.classList.add('blur3px');
+                }
+                // selectRainFall.classList.add('active-rain');
+                // selectDrop.style.animationDuration = Math.random() * 1 + 's';
+            }
+
+            else if (timeSkills === 40) {
                 selectRainy.classList.remove('active-background-gray');
                 selectRainy.classList.add('inactive-background-gray');
+                // selectRainFall.classList.remove('active-rain');
+                // waterDrop.remove();
             }
-
-            // else if (timeSkills === 30) {
-            //     displayDay();
-            // }
-
-            // else if (timeSkills === 35) {
-            //     selectLight.classList.remove('active-light');
-            //     selectLight.classList.add('inactive-light');
-            // }
 
             else if (timeSkills > 50) {
                 timeSkills = 1;
+            }
+
+            else {
+                // selectRainFall.style.opacity = '0';
             }
         }
         
@@ -57,9 +90,10 @@ const Skills = () => {
     return (
         <div className="skills-container">
             <div className="background-picture">
-                <img src={vietnam} alt="" />
+                <img src={vietnam} alt="" className='bg-img' />
                 <img src={vietnamCloud} alt="" className='light-cloud'/>
-                {/* <img src={rain} alt="" /> */}
+                <div className="background-rain">
+                </div>
                 <div className="background-gray"></div>
             </div>
             <div className="foreground-content">
